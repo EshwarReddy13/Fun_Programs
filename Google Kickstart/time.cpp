3e34d15d2f30eeac084d4785a94b3c640a7baca2
EshwarReddy13 <eshwarreddygadi@gmail.com>
2022-03-23 02:50:58 -0500

    enter

---
+++ Google Kickstart/time.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+// int main(){
+
+//     string a,b;
+//     cin >> a >> b;
+//     bool pos = true;
+//     int num;
+//     int j=0;
+//     for(int i =0;i<b.length();++i){
+
+//         if(a.at(j) == b.at(i)){
+//             pos = true;
+//             j+=1;
+//         }else{
+//             pos = false;
+//             num+=1;
+//         }
+//     }
+//     if(pos){
+//         cout << num;
+//     }else{
+//         cout << "IMPOSSIBLE";
+//     }
+//     return 0;
+// }
+int main(){
+    int a,b;
+    cin >> a >>b;
+    for(int i=a;i<=b;i++){
+        
+    }
+    return 0;
+}
---
+++ pass.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin >>a;
+    int arr[3][3];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<a;j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    return 0;
+}
