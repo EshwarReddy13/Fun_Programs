2641c4e8ac5cb057f999c5dbbd20180923c1845f
EshwarReddy13 <eshwarreddygadi@gmail.com>
2022-04-03 18:02:51 -0500

    enter

--- pass.cpp
+++ pass.cpp
@@ -1,16 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
+cannotOpen(maxRow,maxCol,player);
 
-int main(){
+    //Testing loadLevel
+    testLoadlevel("loadlevel1.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel2.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel3.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel4.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel5.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel6.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel7.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel8.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel9.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel10.txt",maxRow,maxCol,player);
+    testLoadlevel("loadlevel11.txt",maxRow,maxCol,player);
 
-    int a;
-    cin >>a;
-    int arr[3][3];
-    for(int i=0;i<a;i++){
-        for(int j=0;j<a;j++){
-            cin >> arr[i][j];
-        }
-    }
-    
-    return 0;
+    //Testing getDirection
+    testGetdirection('w', 1, 2)
+
+    void cannotOpen(int maxRow,int maxCol,Player player){
+    loadLevel("HAHAHA.txt", maxRow, maxCol,player);
+}
+void testLoadlevel(const std::string& fileName,int& maxRow,int& maxCol,Player& player){
+    loadLevel(fileName, maxRow, maxCol,player);
+}
+void testGetdirection(char& input,int& nextRow,int& nextCol){
+    getDirection(input,nextRow,nextCol);   
 }
